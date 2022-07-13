bdfa79b1abe9e9e59affcefa564c255f99134906
echo <heart0magic@163.com>
2022-07-13 21:44:27 +0800

    Complete the navbar css part

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,25 +1,33 @@
 import React from 'react'
-import { Routes,Route,BrowserRouter } from 'react-router-dom'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
 import Dashboard from './pages/Dashboard/Dashboard'
+import Setting from './pages/Setting/Setting'
 import Expense from './pages/Expenses/Expenses'
-import Signin from './pages/Login/Signin'
-import Signup from './pages/Login/Signup'
+import Signin from './pages/login/Signin'
+import Signup from './pages/login/Signup'
 
 export default function App() {
-  return (
-    <BrowserRouter>
-    <div className='app'>
-        <Navbar />
-        {/* for debug at current stage :  */}
-        <Routes>
-            <Route path="/" component={ <Dashboard /> } />
-            <Route path="/expense" component={ <Expense /> }/>
-            <Route path="login" element={<Signin />} />
-            <Route path="sign-up" element={<Signup />} />
-        </Routes>
-    </div>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <div className='app'>
+                <div className="app__body">
+                    <Navbar />
+                    {/* for debug at current stage :  */}
+                    <div className='app__body__routes'>
+                        <Routes >
+                            <Route path="/" element={<Dashboard />} />
+                            <Route path="expense" element={<Expense />} />
+                            <Route path="setting" element={<Setting />} />
+                            <Route path="login" element={<Signin />} />
+                            <Route path="sign-up" element={<Signup />} />
+                        </Routes>
+                    </div >
+
+                </div>
+
+            </div>
+        </BrowserRouter>
+    )
 }
 
