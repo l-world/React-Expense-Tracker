7be405d084b9bf8af3959bd478efd2121455d430
KainiChang <changkaini91@gmail.com>
2022-07-13 17:28:31 +0930

    first try

--- expense-tracker/src/pages/Setting/Setting.js
+++ expense-tracker/src/pages/Setting/Setting.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Signup() {
+function Setting() {
   return (
     <div>
       
@@ -8,4 +8,4 @@ function Signup() {
   )
 }
 
-export default Signup
+export default Setting
