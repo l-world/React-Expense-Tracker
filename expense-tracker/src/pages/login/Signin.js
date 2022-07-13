42cf7a410e7cebda654c839c19ec0c1231313d80
KainiChang <changkaini91@gmail.com>
2022-07-13 17:06:46 +0930

    merge updated main

---
+++ expense-tracker/src/pages/login/Signin.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './sign.css';
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans&display=swap" rel="stylesheet">
+
+function Signin() {
+    return (
+        <div className='sign'>
+            <div className='sign-main'>
+                <img className='sign-logo' src='./images/logo.png' alt='logo'/>
+                <h1>Welcome back</h1>
+                <span className='sign-logo-slogan'>Welcome back! Please enter your details</span>
+                <form className='sign-form'>
+                    <span> Email</span>
+                    <input className='form-text' placeholder='Enter your email' />
+                    <span>Password</span>
+                    <input className='form-text' type="password"/>      
+                    <div className='form-hint1'>
+                        <div>
+                        <input className='checkbox' type="checkbox"  id='remember'name="remember"  />
+                        <label htmlFor="remember">Remember for 30 Days</label>
+                    </div>
+                    <span> Forget password</span>
+                        </div>       
+                        <button>Sign in</button>                          
+                </form>
+                <div className='form-hint2'>
+                    <span>Don’t have an account?</span>
+                    <span> Sign up for free</span>
+                </div>
+            </div>
+            <img className='sign-image' src='./images/sign.png' alt='sign'/>
+        </div>
+    )
+}
+
+export default Signin
