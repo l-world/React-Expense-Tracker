0b7422d0a53f9906aa3e14bf2aa97ef869d451a8
echo <heart0magic@163.com>
2022-07-16 21:28:32 +0800

    change app.js file

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -6,32 +6,32 @@ import Setting from './pages/Setting/Setting'
 import Expense from './pages/Expenses/Expenses'
 import Signin from './pages/login/Signin'
 import Signup from './pages/login/Signup'
-import {AuthContextProvider} from "./components/AuthContext"
+import { AuthContextProvider } from "./components/AuthContext"
 import PrivateRoute from "./components/PrivateRoute"
 import ForgetPW from './pages/login/ForgetPW'
 
 export default function App() {
     return (
         <BrowserRouter>
-        <AuthContextProvider>
-            <div className='app'>
-                <div className="app__body">
-                    <Navbar />
-                    {/* for debug at current stage :  */}
-                    <div className='app__body__routes'>
-                        <Routes >
-                            <Route path="/" element={<Dashboard />} />
-                            <Route path="expense" element={<Expense />} />
-                            <Route path="setting" element={<PrivateRoute><Setting /></PrivateRoute>} />
-                            <Route path="login" element={<Signin />} />
-                            <Route path="sign-up" element={<Signup />} />
-                            <Route path="forgetPassWord" element={<ForgetPW />} />
-                        </Routes>
-                    </div >
+            <AuthContextProvider>
+                <div className='app'>
+                    <div className="app__body">
+                        <Navbar />
+                        {/* for debug at current stage :  */}
+                        <div className='app__body__routes'>
+                            <Routes >
+                                <Route path="/" element={<Dashboard />} />
+                                <Route path="expense" element={<Expense />} />
+                                <Route path="setting" element={<PrivateRoute><Setting /></PrivateRoute>} />
+                                <Route path="login" element={<Signin />} />
+                                <Route path="sign-up" element={<Signup />} />
+                                <Route path="forgetPassWord" element={<ForgetPW />} />
+                            </Routes>
+                        </div >
+
+                    </div>
 
                 </div>
-
-            </div>
             </AuthContextProvider>
         </BrowserRouter>
     )
