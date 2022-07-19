a4be309470efa67aad0162eb29e64a9f7dbacb0c
KainiChang <changkaini91@gmail.com>
2022-07-19 22:57:19 +0930

    finish first time editing profile in Setting

--- expense-tracker/src/components/AuthContext.js
+++ expense-tracker/src/components/AuthContext.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { auth, provider, db } from '../firebase-config'
+import { auth, provider} from '../firebase-config'
 import {
     createUserWithEmailAndPassword,
     onAuthStateChanged,
@@ -11,16 +11,6 @@ import {
 import { updateProfile } from "firebase/auth";
 import { useNavigate } from 'react-router-dom';
 
-import {
-    // collection,
-    // getDocs,
-    doc,
-    // updateDoc,
-    setDoc,
-    // query,
-    // onSnapshot
-} from 'firebase/firestore'
-
 
 const AuthContext = React.createContext()
 
@@ -31,12 +21,11 @@ export function useAuth() {
 
 export function AuthContextProvider({ children }) {
 
-
     const [currentUser, setCurrentUser] = React.useState({
-        displayName:'',
-        email:'',
+        displayName: '',
+        email: '',
         // photoURL:'',
-        uid:'',
+        uid: '',
 
     });
     const [loading, setLoading] = React.useState(true);
@@ -60,11 +49,11 @@ export function AuthContextProvider({ children }) {
     function login(email, password) {
         console.log("logining")
         signInWithEmailAndPassword(auth, email, password)
-        .then(() => {
-            console.log("signed in")
-            navigate('/')
-        })
-        .catch((error) => console.log(error))
+            .then(() => {
+                console.log("signed in")
+                navigate('/')
+            })
+            .catch((error) => console.log(error))
     }
 
     function logout() {
@@ -83,71 +72,28 @@ export function AuthContextProvider({ children }) {
         // })
         // .catch((err)=>console.log(err))
     }
-
-    const [userData, setUserData] = React.useState({
-        id: '',
-        firstName: '',
-        lastName: '',
-        dateofBirth: '',
-        mobilePhone: '',
-        email: ''
-    })
-
-
     React.useEffect(() => {
         onAuthStateChanged(auth, (user) => {
-            if (user !== null){console.log(`auth change:${user.uid}   ${user.displayName}`)}
-            else{console.log("auth change: null")}
+            if (user !== null) { console.log(`auth change:${user.uid}   ${user.displayName}`) }
+            else { console.log("auth change: null") }
             setCurrentUser(user);
             setLoading(false);
-            if (user !== null) {
-                setCurrentUser((prev)=>({
-                    ...prev,
-                    displayName: user.displayName,
-                    email: user.email,
-                    // photoURL: user.photoURL,
-                    uid : user.uid
-                }))
-            }
+            // if (user !== null) {
+            //     setCurrentUser((prev)=>({
+            //         ...prev,
+            //         displayName: user.displayName,
+            //         email: user.email,
+            //         // photoURL: user.photoURL,
+            //         uid : user.uid
+            //     }))
+            // }
         })
     }, []);
 
-    function createData(userData) {
-        const id = currentUser.uid;
-        const newData = {
-            uid: id,
-            firstName: userData.firstName,
-            lastName: userData.lastName,
-            dateofBirth: userData.dateofBirth,
-            mobilePhone: userData.mobilePhone,
-            email: userData.email
-        }
-        setDoc(doc(db, `${id}-data`, id), newData)
-    }
 
-    //get realtime user data after page refresh
-    // const dataRef = collection(db, "userData");
-    // const remoteData = query(dataRef);
-    // React.useEffect(() => {
-    //     onSnapshot(remoteData, async () => {
-    //         const remoteDoc = await getDocs(remoteData);
-    //         const remoteUserData = remoteDoc.docs.map((doc) => doc.data());
-    //         setUserData(remoteUserData);
-    //     });
-    // }, []);
-
-    function handleEditSubmit(userData) {
-        console.log(userData)
-        // updateDoc(userData)
-    }
-
-
-    // onAuthStateChanged(auth,(user)=>{
-    //     if(user){
-    //         getUserName = auth.currentUser.displayName;s
 
     return (
-        <AuthContext.Provider value={{ currentUser, signup, login, logout, loginGoogle, forgetPassWord, userData, setUserData, handleEditSubmit, createData }}>
+        <AuthContext.Provider value={{ currentUser, signup, login, logout, loginGoogle, forgetPassWord }}>
             {!loading && children}
         </AuthContext.Provider>
     );
--- expense-tracker/src/pages/Setting/Setting.js
+++ expense-tracker/src/pages/Setting/Setting.js
@@ -2,10 +2,58 @@ import React from 'react'
 import './setting.css'
 import Topbar from '../../components/Topbar/Topbar'
 import { useAuth } from '../../components/AuthContext'
+import { db } from '../../firebase-config'
+
+import {
+  // collection,
+  // getDocs,
+  doc,
+  // updateDoc,
+  setDoc,
+  // query,
+  // where,
+  // onSnapshot
+} from 'firebase/firestore'
 
 function Setting() {
-  const { currentUser,userData,setUserData,handleEditSubmit,createData} = useAuth()
+  const { currentUser} = useAuth()
   const [editState, setEditState] = React.useState(false)
+
+      //user profile setting (not login things)
+      const [userData, setUserData] = React.useState({
+        id: currentUser.uid,
+        firstName: currentUser.displayName,//如果cloud已经有了firsrname的值，优先展示，怎么从firebase取到这个值？
+        lastName: '',
+        dateOfBirth: '',
+        mobilePhone: '',
+        email: currentUser.email////如果cloud已经有了email的值，优先展示，怎么从firebase取到这个值？
+    })
+    const id = currentUser.uid;
+    // const userProfileRef = collection(db, "userProfile");
+    const docRef=doc(db, "userProfile", id)
+    // const userCloudData = getDocs(docRef);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setEditState(false)
+    updateData(userData)
+  }
+
+    function updateData(userData) {
+        setDoc(docRef, userData)
+    }
+
+    //get realtime user data after page refresh
+    // React.useEffect(() => {
+    //         const remoteDoc =  getDocs(userCloudData);
+    //         const remoteUserData = remoteDoc.data()
+    //         setUserData(remoteUserData);
+    // }, [userCloudData]);
+
+    // function handleEditSubmit(userData) {
+    //     console.log(userData)
+    //     // updateDoc(userData)
+    // }
 
   function handleEdit() {
     setEditState(true)
@@ -16,15 +64,9 @@ function Setting() {
     setUserData((prev) => ({ ...prev, [name]: value }))
   }
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    setEditState(false)
-    userData.id?
-    handleEditSubmit(userData)  
-    : createData(userData)
-  }
+  function handlePWChange(){}
 
-
+  function confirmPWChange(){}
 
   return (
     <main className='dashboard'>
@@ -44,35 +86,33 @@ function Setting() {
           </div>
           <div className='setting__main__content_4'>
             <span> First Name</span>
-            <input className='form-text' name="displayName-first" disabled={!editState} onChange={handleChange} placeholder={currentUser.displayName || 'Enter your full name'} />
-            {/* onBlur={isValidName} */}
+            <input className='form-text' name="firstName" disabled={!editState} onChange={handleChange} value={userData.firstName || 'Enter your full name'} />
           </div>
           <div className='setting__main__content_4'>
             <span> Last Name</span>
-            <input className='form-text' name="displayName-last" disabled={!editState} onChange={handleChange} placeholder='Enter your full name' />
-            {/* onBlur={isValidName} */}
+            <input className='form-text' name="lastName" disabled={!editState} onChange={handleChange} placeholder='Enter your full name' />
           </div>
           <div className='setting__main__content_4'>
             <span> Date of Birth</span>
-            <input className='form-text' name="DateOfBirth" type="date" disabled={!editState} onChange={handleChange} />
+            <input className='form-text' name="dateOfBirth" type="date" disabled={!editState} onChange={handleChange} />
           </div>
           <div className='setting__main__content_4'>
             <span> Mobile Number</span>
-            <input className='form-text' name="Mobile" type="tel" disabled={!editState} onChange={handleChange} placeholder='Mobile number...' />
+            <input className='form-text' name="mobilePhone" type="tel" disabled={!editState} onChange={handleChange} placeholder='Mobile number...' />
           </div>
           <div className='setting__main__content_5'>
             <span> Email</span>
-            <input className='form-text' type="email" name="email" disabled={!editState} onChange={handleChange} placeholder={currentUser.email} />
+            <input className='form-text' type="email" name="email" disabled={!editState} onChange={handleChange} value={userData.email} />
             {/*onBlur={isValidEmail}  */}
           </div>
           <div className='setting__main__content_4'>
             <span>New Password</span>
-            <input className='form-text' type="password" name="NewPW" disabled={!editState} onChange={handleChange} placeholder='Enter your password' />
+            <input className='form-text' type="password" name="NewPW" disabled={!editState} onChange={handlePWChange} placeholder='Enter your password' />
             {/* onBlur={isValidPassword} */}
           </div>
           <div className='setting__main__content_4'>
             <span>Confirm Password</span>
-            <input className='form-text' type="password" name="ConfirmPW" disabled={!editState} onChange={handleChange} placeholder='Enter your password' />
+            <input className='form-text' type="password" name="ConfirmPW" disabled={!editState} onChange={confirmPWChange} placeholder='Enter your password' />
             {/*onBlur={isSamePassword} */}
           </div>
           <div className='setting__main__content_4'>
